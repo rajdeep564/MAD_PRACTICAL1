bfc8bfad516ad6ddbeef302eb84d4b99bdbc8b54
Rajdeep <rajdeepsinghchavda7@gmail.com>
2023-07-18 10:46:54 +0530

    till 1.3

---
+++ src/main/kotlin/1.4.kt
@@ -0,0 +1,14 @@
+fun main(){
+    println("Student Enrollment no : -")
+    val Student_enrollment = readLine()!!
+    println("Student Name : -")
+    val Student_name = readLine()!!
+    println("Student Branch : -")
+    val Student_branch = readLine()!!
+    println("Student Class : -")
+    val Student_class = readLine()!!
+    println("Student College Name : -")
+    val Student_clgname = readLine()!!
+
+
+}
